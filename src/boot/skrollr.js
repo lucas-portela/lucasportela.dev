0eddc3ca90e94b0b74d6a2895ea242bbc092ebb3
Lucas Portela <developer@lucasportela.dev>
2021-10-08 01:48:48 -0300

    Adaptando ao telemóvelzito

--- src/boot/skrollr.js
+++ src/boot/skrollr.js
@@ -1,11 +1,19 @@
 import { boot } from "quasar/wrappers";
 import _skrollr from "../lib/skrollr";
+import { EventEmitter } from "events";
 
 const skrollr = _skrollr.init();
-
+skrollr.keyframeEvt = new EventEmitter();
+skrollr.on("keyframe", (element, name, direction) => {
+  if (!element) return;
+  const id = element.getAttribute("data-id") || "--";
+  skrollr.keyframeEvt.emit(`${id}.${name}.${direction}`);
+});
 export default boot(({ app }) => {
-  window.skrollr = _skrollr + 1;
+  window.skrollr = skrollr;
+
   app.config.globalProperties.$skrollr = skrollr;
+  app.config.globalProperties.$skrollr;
   // app.config.globalProperties.$skrollr = {
   //   refresh() {},
   // };
--- src/components/TerminalIntro.vue
+++ src/components/TerminalIntro.vue
@@ -6,29 +6,32 @@
     background="primary-3"
     color="white"
     parallax="https://images.pexels.com/photos/2767814/pexels-photo-2767814.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-    :dense="true"
+    :dense="false"
   >
     <div class="flex flex-row justify-center">
       <div
         ref="termynal"
+        data-emit-events
+        data-id="termynal"
+        @click="showDetails()"
         :data-0p="
           $skipInMobile(
-            'filter: blur(10px); opacity: 0; transform: translateY(100vh) scale(0.7);'
+            'filter: blur(10px); opacity: 0; transform: translateY(100vh) scale(0.5);'
           )
         "
-        :data-100p="
+        :data-80p="
           $skipInMobile(
             'filter: blur(0px); opacity: 1; transform: translateY(0vh) scale(1);'
           )
         "
-        :data--30p-bottom="
+        :data--60p-bottom="
           $skipInMobile(
             'filter: blur(0px); opacity: 1; transform: translateY(0vh) scale(1);'
           )
         "
-        :data--80p-bottom="
+        :data--120p-bottom="
           $skipInMobile(
-            'filter: blur(10px); opacity: 1; transform: translateY(200vh)  scale(0.7);'
+            'filter: blur(10px); opacity: 1; transform: translateY(200vh)  scale(0.5);'
           )
         "
         class="termynal shadow-12 bg-white"
@@ -40,7 +43,7 @@
 <style lang="scss">
 .terminal-intro {
   .termynal {
-    min-height: 200vh !important;
+    min-height: 50vh !important;
   }
 }
 </style>
@@ -55,6 +58,8 @@ export default {
   },
   data: () => ({
     termynal: null,
+    keyframeEvent: "termynal.data80p.down",
+    state: 0,
     summary:
       "Web App Development, Mobile App Development, Geolocation, RFID Systems, Web Scrapping, IA and Computer Vision.",
     skills: [
@@ -75,23 +80,62 @@ export default {
       "Arduino, Raspberry Pi and basic eletronics knowledge for hardware prototyping",
     ],
   }),
+  computed: {
+    touchText() {
+      return this.$q.platform.is.mobile ? "Touch" : "Click";
+    },
+  },
   mounted() {
-    $(window).on("scroll", this.scrollHandler);
+    this.$skrollr.keyframeEvt.on(this.keyframeEvent, this.skrollrListener);
   },
   beforeUnmount() {
-    this.removeScrollHandler();
+    this.removeSkrollrListener();
   },
   methods: {
-    removeScrollHandler() {
-      $(window).off("scroll", this.scrollHandler);
+    removeSkrollrListener() {
+      this.$skrollr.keyframeEvt.off(this.keyframeEvent, this.skrollrListener);
     },
-    scrollHandler() {
-      console.log("handling scroll");
-      const target = $(this.$refs.termynal).offset().top;
-      const current = $(window).scrollTop();
-      if (Math.abs(target - current) < 300) {
+    skrollrListener() {
+      if (this.state != 0) return this.removeSkrollrListener();
+      this.state = 1;
+      $(this.$refs.termynal).find("*").remove();
+      this.termynal = new Termynal(this.$refs.termynal, {
+        callback: () => {
+          this.$skrollr.refresh();
+          this.state = 2;
+        },
+        lineDelay: 50,
+        progressLength: 18,
+        typeDelay: 10,
+        lineData: [
+          {
+            type: "input",
+            prompt: "guest@shiwak:~$",
+            value: "skillset --summary",
+          },
+          { type: "progress" },
+          { prompt: "Summary: ", value: this.summary },
+          {
+            prompt: "Help: ",
+            value:
+              this.touchText + " in this terminal to show more information.",
+          },
+        ],
+      });
+    },
+    showDetails() {
+      if (this.state == 4) {
+        this.state = 0;
+        this.skrollrListener();
+        this.$scrollTo("#letsbashmeout");
+      } else if (this.state == 2) {
+        this.state = 3;
+        $(this.$refs.termynal).find("*").remove();
         this.termynal = new Termynal(this.$refs.termynal, {
-          callback: () => this.$skrollr.refresh(),
+          callback: () => {
+            this.$skrollr.refresh();
+            this.state = 4;
+          },
           lineDelay: 50,
           progressLength: 18,
           typeDelay: 10,
@@ -102,11 +146,18 @@ export default {
               value: "skillset --list",
             },
             { type: "progress" },
-            { prompt: "Summary: ", value: this.summary },
-            ...this.skills.map((skill) => ({ prompt: "-> ", value: skill })),
+            ...this.skills.map((skill) => ({
+              prompt: "-> ",
+              value: skill,
+            })),
+            {
+              prompt: "Help: ",
+              value:
+                this.touchText +
+                " in this terminal to show summary information.",
+            },
           ],
         });
-        this.removeScrollHandler();
       }
     },
   },
