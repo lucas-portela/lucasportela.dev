fefb9d0818c9f514df9cc70ce2c4bb47742f65d1
Lucas Portela <developer@lucasportela.dev>
2021-10-22 20:10:47 -0300

    Added experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,26 +1,97 @@
 export default [
   {
-    year: "2018",
-    month: "August",
+    year: "2013",
+    month: "Jan",
     role: "Trainee",
-    duration: "2 months",
-    title: "Lorem Ipsum",
-    description: "Lorem ipsum.",
+    duration: "3 Years",
+    title: "CPAI - University of Brasilia (UnB)",
+    description: `Lead by PhD Mamede Lima-Marques, CPAI (in english IARC - Information Architecture Research Center) is a research center at University of Brasilia
+      where the main field of study is Information Architecture. During my 3 years as a trainee,
+      I had personal Formal Logic classes with Mamede and other academics. I also researched
+      and developed 2 projects in the field with functional prototypes, ALP (in english PLA - Propositional Logic Analyzer) and LOG++, wich I presented
+      in the V Colloquy of Information Architecture, an event that occurred in 2015 at UnB.
+      The project LOG++ was adapted by me and evolved to a new project whose name was SARC (in english ASCR - Accessible
+        System of Cognitive Robotics). SARC was the  presented in the V MNR
+        (in english 5th National Robotics Show) representing UnB.`,
+  },
+  {
+    year: "2016",
+    month: "March",
+    role: "Full Stack Web Developer",
+    duration: "11 months",
+    title: "Freelance",
+    description: `After the end of my trainee contract, I've launched myself in the freelance market working in
+      low budget projects to get experience and to build some networking. During this time I've used
+      Java EE, PHP, MySQL, Web-design, social media and Search Engine Optimization. My most remarkable
+      work in this period was the development of a realtime multiplayer card game using a development stack build over WebSockets, Java EE and HTML5.`,
+  },
+  {
+    year: "2017",
+    month: "May",
+    role: "Educational Robotics Teacher and Full Stack Developer",
+    duration: "6 months",
+    title:
+      "Unity Escola de Programação e Robótica (Not related to Unity 3D, the game engine)",
+    description: `Not related to Unity 3D, the game engine, this company was a school focused in
+    educational robotics for kids. As a developer I was responsible for creating a system to manage
+    teachers, students and the bills for the parents. As a teacher, I was responsible for planning
+    classes and creating robotic kits.`,
   },
   {
     year: "2018",
-    month: "Feb",
-    role: "Trainee",
-    duration: "2 months",
-    title: "Lorem Ipsum",
-    description: "Lorem ipsum.",
+    month: "Apr",
+    role: "Front-end Developer",
+    duration: "3 months",
+    title: "Boa Imagem - Digital Marketing Agency",
+    description: `When we talk about digital marketing agencies, sometimes there is no much space for
+    development, back-end stuff and things get a bit mechanical and repetitive. So I didn't spent much
+    time here. Was I good experience since I had contact with the leading CMS softwares in that epoch
+    (WordPress and Drupal).`,
+  },
+  {
+    year: "2018",
+    month: "Oct",
+    role: "Full Stack Developer",
+    duration: "6 months",
+    title: "AXSAT - Vehicle Tracking and Monitoring",
+    description: `AXSAT was created just before I came and I was responsible
+    for developing all the technology from scratch. I built the back-end on top of Traccar, an open-source
+    tracking system written in Java. With this stable back-end as starting point, I wrote the protocol
+    implementations for Suntech and GlobalStar trackers. I also wrote some modifications, adding new features 
+    in the core of Traccar to supply business specific demands, including integrations with firebase APIs
+    and remodeling entities of the database. When I left the company, being almost the only one active
+    developer, I left them with a complete functional product, wich they use till today.`,
   },
   {
     year: "2019",
-    month: "May",
-    role: "Trainee",
-    duration: "2 months",
-    title: "Lorem Ipsum",
-    description: "Lorem ipsum.",
+    month: "Aug",
+    role: "Full Stack Developer",
+    duration: "3 months",
+    title: "LinkData S/A - IT Solutions for Public Administration ",
+    description: `Few months later I was hired by LinkData through the networking I built at AXSAT.
+    They had some big IT contracts with the government of Brazil and their tech stack was a little
+    outdated when I came there. So my mission was to bring innovation to the new products, since I was
+    an experienced javaScript developer, working with reactive layouts, realtime database and stuffs like
+    this. The first and only project I built there was a system responsible for managing  the logistics
+    inside a shed. The system was composed of a digital totem and a back-end service connected to RFID
+    readers through MQTT subscriptions. Before completing 3 months and having the project done, I got a
+    proposal from another company offering a higher payment and so I aggreed to work to them leaving
+    LinkData.`,
+  },
+  {
+    year: "2019",
+    month: "Aug",
+    role: "Full Stack Developer",
+    duration: "3 months",
+    title: "LinkData S/A - IT Solutions for Public Administration ",
+    description: `Few months later I was hired by LinkData through the networking I built at AXSAT.
+    They had some big IT contracts with the government of Brazil and their tech stack was a little
+    outdated when I came there. So my mission was to bring innovation to the new products, since I was
+    an experienced javaScript developer, working with reactive layouts, realtime database and stuffs like
+    this. The first and only project I built there was a system responsible for managing  the logistics
+    inside a shed. The system was composed of a digital totem and a back-end service connected to RFID
+    readers through MQTT subscriptions. Before completing 3 months and having the project done, I got a
+    proposal from another company offering a higher payment and so I aggreed to work to them leaving
+    LinkData.`,
   },
 ];
