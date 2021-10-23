2cf389793e2ab43e5dc5a1db105242cd3b3a97a1
Lucas Portela <developer@lucasportela.dev>
2021-10-22 21:00:21 -0300

    Finished Experience Section

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,7 +1,7 @@
 export default [
   {
     year: "2013",
-    month: "Jan",
+    month: "January",
     role: "Trainee",
     duration: "3 Years",
     title: "CPAI - University of Brasilia (UnB)",
@@ -28,7 +28,7 @@ export default [
   {
     year: "2017",
     month: "May",
-    role: "Educational Robotics Teacher and Full Stack Developer",
+    role: "Teacher and Full Stack Developer",
     duration: "6 months",
     title:
       "Unity Escola de Programação e Robótica (Not related to Unity 3D, the game engine)",
@@ -39,7 +39,7 @@ export default [
   },
   {
     year: "2018",
-    month: "Apr",
+    month: "April",
     role: "Front-end Developer",
     duration: "3 months",
     title: "Boa Imagem - Digital Marketing Agency",
@@ -50,11 +50,11 @@ export default [
   },
   {
     year: "2018",
-    month: "Oct",
+    month: "October",
     role: "Full Stack Developer",
     duration: "6 months",
     title: "AXSAT - Vehicle Tracking and Monitoring",
-    description: `AXSAT was created just before I came and I was responsible
+    description: `AXSAT was created just before I came, so I was responsible
     for developing all the technology from scratch. I built the back-end on top of Traccar, an open-source
     tracking system written in Java. With this stable back-end as starting point, I wrote the protocol
     implementations for Suntech and GlobalStar trackers. I also wrote some modifications, adding new features 
@@ -64,34 +64,45 @@ export default [
   },
   {
     year: "2019",
-    month: "Aug",
+    month: "August",
     role: "Full Stack Developer",
     duration: "3 months",
     title: "LinkData S/A - IT Solutions for Public Administration ",
     description: `Few months later I was hired by LinkData through the networking I built at AXSAT.
     They had some big IT contracts with the government of Brazil and their tech stack was a little
-    outdated when I came there. So my mission was to bring innovation to the new products, since I was
-    an experienced javaScript developer, working with reactive layouts, realtime database and stuffs like
-    this. The first and only project I built there was a system responsible for managing  the logistics
+    outdated when I came there. So my mission was to bring innovation to new products, since I was
+    an experienced JavaScript developer, working with reactive layouts, realtime database and stuff like
+    that. The first and only project I built there was a system responsible for managing  the logistics
     inside a shed. The system was composed of a digital totem and a back-end service connected to RFID
     readers through MQTT subscriptions. Before completing 3 months and having the project done, I got a
-    proposal from another company offering a higher payment and so I aggreed to work to them leaving
+    invite from another company offering a higher payment and so I aggreed to work to them and leave
     LinkData.`,
   },
   {
     year: "2019",
-    month: "Aug",
+    month: "November",
     role: "Full Stack Developer",
-    duration: "3 months",
-    title: "LinkData S/A - IT Solutions for Public Administration ",
-    description: `Few months later I was hired by LinkData through the networking I built at AXSAT.
-    They had some big IT contracts with the government of Brazil and their tech stack was a little
-    outdated when I came there. So my mission was to bring innovation to the new products, since I was
-    an experienced javaScript developer, working with reactive layouts, realtime database and stuffs like
-    this. The first and only project I built there was a system responsible for managing  the logistics
-    inside a shed. The system was composed of a digital totem and a back-end service connected to RFID
-    readers through MQTT subscriptions. Before completing 3 months and having the project done, I got a
-    proposal from another company offering a higher payment and so I aggreed to work to them leaving
-    LinkData.`,
+    duration: "9 months",
+    title: "Synapse Brasil",
+    description: `After leaving LinkData I was hired by Synapse Brasil, a Software Factory where I met most
+    of the devs who I admire and learned from. Till now I was always running against deadlines, delivering
+    things in record time and dealing with the arbitrary desires of unaware clients by my own, without a team.
+    But at Synapse Brasil I was part of a team for the first time and I had time to evolve my view and ideas
+    on software engineering. There I worked with NodeJS, Laravel, MongoDB, Angular, Vue, Payment Processing,
+    routine automation and made some friends.`,
+  },
+  {
+    year: "2020",
+    month: "July",
+    role: "Full Stack Developer",
+    duration: "Current Job",
+    title: "ASI Company",
+    description: `In July 31, ASI Company sent me an e-mail with a job offer. In te middle of an economic
+    and health crisis, one that was severe in Brasil, I realized the need to have some labor rights, the ones
+    that are very rare in IT companies here. Some of wich ASI Company could give, so I jumped in and here
+    I am till today. At ASI Company I work at the innovation team. Some of my recent impact at the company
+    was my expertise developing a BPMN process system prototype in 5 weeks, which was crucial to the
+    negociation of a deal with the government of Dominican Republic.
+     `,
   },
 ];
